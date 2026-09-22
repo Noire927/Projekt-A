ee555ff516874b31836afacceba3411d513a3cd7
Noire927 <Morten927@hotmail.com>
2026-09-22 18:43:05 +0200

    lavet variable med ordene fra arrayet og de blandede ord og lavet en prompts, hvor ordene ska lgættes"

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const ord = [
   "MICA",
   "Computer Science",
   "Spiludvikler",
-  "Girls frontline",
+  "Girls Frontline",
   "Tololo",
   "SOPMOD II",
   "HK416",
   "Druid",
   "Forever",
 ];
+
+let randomOrd = ord[Math.floor(Math.random() * ord.length)];
+let bogstaver = randomOrd.split("");
+bogstaver.sort(() => Math.random() - 0.5); // - 0.5 → gør tallet positivt eller negativt
